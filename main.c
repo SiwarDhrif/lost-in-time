67ae04edc66ad5da96710aead68d1da81fa88638
siwar-11 <siwar.dhrif@esprit.tn>
2020-06-16 22:49:17 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_rotozoom.h>
+#include "rotozoom.h"
+#include "SDL2_rotozoom.h"
+
+
+int main(int argc, char *argv[])
+{
+
+ rotozoom();
+
+
+ SDL_Quit();
+
+return 0;
+}
+
